a8ab94e5605312f08267a1b35d6c282d76f9d171
Jaiwa Team <jaiwagum@gmail.com>
2026-07-16 06:14:18 +0630

    feat(callbackHandler): implement handleCallback function for processing callback queries

---
+++ src/handlers/callbackHandler.js
@@ -0,0 +1,14 @@
+
+export async function handleCallback(query, env) {
+    const data = query.data;
+
+    switch (data) {
+
+        case "copy":
+            return;
+
+        default:
+            return;
+
+    }
+}
--- src/worker.js
+++ src/worker.js
@@ -1,19 +1,21 @@
 import { handleRouter } from "./router.js";
+import { handleCallback } from "./handlers/callbackHandler.js";
 
 export default {
 
     async fetch(request, env) {
 
         if (request.method !== "POST") {
-
             return new Response("GP Garum Bot");
 
         }
 
         const update = await request.json();
-
+        if (update.callback_query) {
+            await handleCallback(update.callback_query, env);
+            return new Response("OK");
+        }
         await handleRouter(update, env);
-
         return new Response("OK");
 
     }
