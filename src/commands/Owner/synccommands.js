e3cb4638877874b70150f30b9f1807756bd9f7e4
Jaiwa Team <jaiwagum@gmail.com>
2026-07-20 02:41:56 +0630

    feat(commands): add private chat commands to syncCommands function

--- src/commands/Owner/synccommands.js
+++ src/commands/Owner/synccommands.js
@@ -1,10 +1,36 @@
 import { callTelegram, sendMessage } from "../../telegram.js";
 
 export async function syncCommands(update, env) {
+
+    // Private Chat Commands
     await callTelegram(
         env,
         "setMyCommands",
         {
+            scope: {
+                type: "all_private_chats"
+            },
+            commands: [
+                {
+                    command: "start",
+                    description: "Start the bot"
+                },
+                {
+                    command: "usage",
+                    description: "How to use the bot"
+                }
+            ]
+        }
+    );
+
+    // Group Commands
+    await callTelegram(
+        env,
+        "setMyCommands",
+        {
+            scope: {
+                type: "all_group_chats"
+            },
             commands: [
                 {
                     command: "calc",
@@ -41,7 +67,7 @@ export async function syncCommands(update, env) {
     return await sendMessage(
         env,
         update.message.chat.id,
-        "<tg-emoji emoji-id='6055095969651892237'>✅</tg-emoji>   Commands synced successfully.",
+        "<tg-emoji emoji-id='6055095969651892237'>✅</tg-emoji> Commands synced successfully.",
         {
             parse_mode: "HTML"
         }
